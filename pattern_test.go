0999526f1ea9a0adbbb20dc1fbb09956dc5ec355
Jacob Dearing <jacob.dearing@gmail.com>
2025-03-08 06:34:40 +0000

    bump up test coverage to 98.1%

--- pattern_test.go
+++ pattern_test.go
@@ -9,17 +9,20 @@ func TestRegexp(t *testing.T) {
 	db.Set("card:1:black_lotus", "Black Lotus")
 	db.Set("card:2:mox_pearl", "Mox Pearl")
 
+	// feed in some bad regexp to MatchKeys
 	badRegexp := "card[0-9"
 	_, err := db.MatchKeys(badRegexp)
 	if err == nil {
 		t.Errorf("expected error, got nil")
 	}
 
+	// feed in some bad regexp to MatchValues
 	_, err = db.MatchValues(badRegexp)
 	if err == nil {
 		t.Errorf("expected error, got nil")
 	}
 
+	// fetch both cards by key
 	keys, err := db.MatchKeys("card:[0-9]:.")
 	if err != nil {
 		t.Error(err)
@@ -28,6 +31,7 @@ func TestRegexp(t *testing.T) {
 		t.Errorf("expected %d, got %d", 2, len(keys))
 	}
 
+	// fetch both cards by value
 	values, err := db.MatchValues("card:[0-9]:.")
 	if err != nil {
 		t.Error(err)
@@ -42,6 +46,7 @@ func TestPrefix(t *testing.T) {
 	db.Set("card:1:black_lotus", "Black Lotus")
 	db.Set("card:2:mox_pearl", "Mox Pearl")
 
+	// fetch one card by prefix exact match
 	keys, err := db.Prefix("card:2:mox_pearl")
 	if err != nil {
 		t.Error(err)
@@ -51,6 +56,7 @@ func TestPrefix(t *testing.T) {
 		t.Errorf("expected %d, got %d", 1, len(keys))
 	}
 
+	// fetch both cards by prefix using wildcards
 	keys, err = db.Prefix("*:?:*")
 	if err != nil {
 		t.Error(err)
@@ -60,7 +66,8 @@ func TestPrefix(t *testing.T) {
 		t.Errorf("expected %d, got %d", 2, len(keys))
 	}
 
-	keysChan := db.PrefixChan("c?")
+	// fetch both cards by prefix to a channel using wildcards
+	keysChan := db.PrefixChan("*:?:*")
 	count := 0
 	for range keysChan {
 		count++
